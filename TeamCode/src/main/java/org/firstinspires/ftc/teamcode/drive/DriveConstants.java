2cefd3768aca1677d6352ba7d1f7b336ddbedd69
Davidvh10 <david2011brian2009@gmail.com>
2024-11-25 19:55:40 -0500

    Update DriveConstants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -99,8 +99,8 @@ public class DriveConstants {
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
 
      */
-    public static double MAX_VEL = 32 ; //26.295 51.1394
-    public static double MAX_ACCEL = 32 ; //52.48180821614297 51.1394
+    public static double MAX_VEL = 40 ; //26.295 51.1394
+    public static double MAX_ACCEL = 40 ; //52.48180821614297 51.1394
     public static double MAX_ANG_VEL = Math.toRadians(208.5244); //141.64
     public static double MAX_ANG_ACCEL = Math.toRadians(184.02607784577722);
 
